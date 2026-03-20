347bd4e8c3a0f55f14670a593eb452a4190b05d8
Samuel Sarle <samuel@sarle.com>
2026-03-21 01:05:27 +0200

    Make Luma `User`'s field `last_online_at` optional

--- python/src/known_types/luma/user.py
+++ python/src/known_types/luma/user.py
@@ -1,7 +1,9 @@
 # This is free and unencumbered software released into the public domain.
 
 from datetime import datetime
+
 from pydantic import BaseModel, ConfigDict
+
 
 # See: https://api2.luma.com/user/profile?username=arto
 class User(BaseModel):
@@ -11,7 +13,7 @@ class User(BaseModel):
     bio_short: str | None = None  # e.g. "Founder, ASIMOV Protocol. ex-NEAR Protocol"
     instagram_handle: str | None = None  # e.g. "arto.bendiken"
     is_verified: bool
-    last_online_at: datetime  # e.g. "2026-02-27T22:30:28.589Z"
+    last_online_at: datetime | None  # e.g. "2026-02-27T22:30:28.589Z"
     linkedin_handle: str | None = None  # e.g. "/in/arto"
     name: str  # e.g. "Arto Bendiken"
     tiktok_handle: str | None = None
