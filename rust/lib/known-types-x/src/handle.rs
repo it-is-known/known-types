e6f313671a58a7e609580a99fc194554facffa5f
Arto Bendiken <arto@bendiken.net>
2026-04-12 20:32:51 -0700

    Derive `From` for `XHandle`.

--- rust/lib/known-types-x/src/handle.rs
+++ rust/lib/known-types-x/src/handle.rs
@@ -4,8 +4,9 @@
 compile_error!("this module requires the 'alloc' feature");
 
 use crate::prelude::String;
-use derive_more::{AsRef, Display, FromStr};
+use derive_more::{AsRef, Display, From, FromStr};
 
-#[derive(AsRef, Clone, Debug, Display, Eq, FromStr, Hash, Ord, PartialEq, PartialOrd)]
+#[derive(AsRef, Clone, Debug, Display, Eq, From, FromStr, Hash, Ord, PartialEq, PartialOrd)]
+#[from(forward)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct XHandle(String);
